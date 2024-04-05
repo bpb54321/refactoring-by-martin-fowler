e008f55d149c9cbe49a8d0fde391f403a28bfb17
Brian Blosser <brian.blosser@outbox.com>
2024-04-05 11:37:03 -0400

    Install and run prettier on project.

--- tests/statement.test.ts
+++ tests/statement.test.ts
@@ -1,11 +1,11 @@
-import {invoices} from "../src/invoices";
-import {plays} from "../src/plays";
-import {statement} from "../src/statement";
+import { invoices } from "../src/invoices";
+import { plays } from "../src/plays";
+import { statement } from "../src/statement";
 
 describe("statement", () => {
   it("should produce the correct statement for invoice 0", () => {
     // Act
-    const result = statement(invoices[0], plays)
+    const result = statement(invoices[0], plays);
 
     // Assert
     expect(result).toMatchSnapshot();
