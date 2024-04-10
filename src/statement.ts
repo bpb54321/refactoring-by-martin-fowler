60f07541097a31028c02fa57801a86c9e73faea4
Brian Blosser <brian.blosser@outbox.com>
2024-04-10 17:50:27 -0400

    Rename return value to result.

--- src/statement.ts
+++ src/statement.ts
@@ -12,25 +12,25 @@ export function statement(invoice: Invoice, plays: Plays) {
   }).format;
 
   function getChargeForPerformance(play: Play, perf: Performance) {
-    let thisAmount = 0;
+    let result = 0;
     switch (play.type) {
       case PlayType.Tragedy:
-        thisAmount = 40000;
+        result = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          result += 1000 * (perf.audience - 30);
         }
         break;
       case PlayType.Comedy:
-        thisAmount = 30000;
+        result = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          result += 10000 + 500 * (perf.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    return thisAmount;
+    return result;
   }
 
   for (let perf of invoice.performances) {
