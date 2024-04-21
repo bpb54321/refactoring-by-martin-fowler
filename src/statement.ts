429be2824702d34c553aa6644ea82d8413dbabcd
Brian Blosser <brian.blosser@outbox.com>
2024-04-21 16:23:07 -0400

    Extract totalVolumeCredits and inline.

--- src/statement.ts
+++ src/statement.ts
@@ -45,6 +45,14 @@ export function statement(invoice: Invoice, plays: Plays) {
     }).format(aNumber / 100);
   }
 
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
   let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
@@ -53,11 +61,8 @@ export function statement(invoice: Invoice, plays: Plays) {
     result += ` ${getPlay(perf).name}: ${usd(getChargeForPerformance(perf))} (${perf.audience} seats)\n`;
     totalAmount += getChargeForPerformance(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
