0b8ebaec930266712efe598e66214f736ecb3d8f
Brian Blosser <brian.blosser@outbox.com>
2024-04-21 16:05:40 -0400

    Extract format function.

--- src/statement.ts
+++ src/statement.ts
@@ -37,14 +37,17 @@ export function statement(invoice: Invoice, plays: Plays) {
     return result;
   }
 
+  function format(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber);
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
